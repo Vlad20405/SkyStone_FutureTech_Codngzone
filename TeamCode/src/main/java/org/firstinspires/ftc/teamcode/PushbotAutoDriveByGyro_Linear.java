28287b9aba53332b0d819f1c732d84b607f6ef41
AndreiP33 <andrei.pata33@gmail.com>
2020-01-17 09:25:36 +0200

    Mama-i virgina

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/PushbotAutoDriveByGyro_Linear.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/PushbotAutoDriveByGyro_Linear.java
@@ -78,7 +78,7 @@ import org.firstinspires.ftc.robotcore.external.navigation.Orientation;
  * Use Android Studios to Copy this Class, and Paste it into your team's code folder with a new name.
  * Remove or comment out the @Disabled line to add this opmode to the Driver Station OpMode list
  */
-
+//mama-i sorata
 @Autonomous(name="Pushbot: Auto Drive By Gyro", group="Pushbot")
 @Disabled
 public class PushbotAutoDriveByGyro_Linear extends LinearOpMode {
