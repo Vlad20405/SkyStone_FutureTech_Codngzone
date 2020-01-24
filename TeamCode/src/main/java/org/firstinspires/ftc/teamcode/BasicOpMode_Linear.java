8a0bfb4154698d975d421dc259c1dea4f7c1deb2
Adrian <adivd15@gmail.com>
2020-01-24 10:28:51 +0200

    Codu cu cutia

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BasicOpMode_Linear.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BasicOpMode_Linear.java
@@ -93,6 +93,8 @@ public class BasicOpMode_Linear extends LinearOpMode {
         waitForStart();
         runtime.reset();
 
+        servo_gimbal_2.setPosition(0);
+
         while (opModeIsActive()) {
 
             //codul pentru mechanum
@@ -106,10 +108,25 @@ public class BasicOpMode_Linear extends LinearOpMode {
             motor_brat.setPower(gamepad2.left_stick_y*0.2);
             motor_cremaliera.setPower(gamepad2.left_trigger);
             motor_cremaliera.setPower(-gamepad2.right_trigger);
-            pozitie_servo1=Range.clip(pozitie_servo1+gamepad2.right_stick_x*0.2,0,1);
+
+            if(gamepad2.a){
+                pozitie_servo2=pozitie_servo2+0.01;
+            }
+            if (gamepad2.b){
+                pozitie_servo2=pozitie_servo2-0.01;
+            }
+            if(gamepad2.x){
+                pozitie_servo1=pozitie_servo1+0.01;
+            }
+            if(gamepad2.y){
+                pozitie_servo1=pozitie_servo1-0.01;
+            }
+
+            pozitie_servo1=Range.clip(pozitie_servo1,0,1);
+            pozitie_servo2=Range.clip(pozitie_servo2,0,1);
+
+            servo_gimbal_2.setPosition(pozitie_servo2);
             servo_gimbal_1.setPosition(pozitie_servo1);
-            pozitie_servo2=Range.clip(gamepad2.right_stick_y*0.2+pozitie_servo2,0,1);
-            servo_gimbal_1.setPosition(pozitie_servo2);
 
             telemetry.addData("Valor",gamepad2.right_stick_x);
             telemetry.addData("Status", "Run Time: " + runtime.toString());
